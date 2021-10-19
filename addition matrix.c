585f6dc7a489a754fef0062984789e79e307478b
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2021-10-20 00:01:13 +0530

    Add files via upload

---
+++ addition matrix.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int r,c,p,q,i,j;
+	printf("enter row and column for first matrix");
+	scanf("%d%d",&r,&c);
+		printf("enter row and column for second matrix");
+	scanf("%d%d",&p,&q);
+	int a[r][c],b[p][q],k[r][c];
+	if(r==p&&c==q)
+	{
+		printf("enter number for first matrix");
+		for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+			//printf("enter number for first matrix");
+			scanf("%d",&a[i][j]);
+		}}
+			printf("enter number for second matrix");
+			for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+			//printf("enter number for second matrix");
+		    scanf("%d",&b[i][j]);
+			
+		}}
+		   	for(i=0;i<r;i++)
+		{
+			for(j=0;j<c;j++)
+			{
+			k[i][j]=a[i][j]+b[i][j];
+			printf("%d",k[i][j]);
+			}
+			printf("\n");
+		}
+	}
+}
+	
+	
